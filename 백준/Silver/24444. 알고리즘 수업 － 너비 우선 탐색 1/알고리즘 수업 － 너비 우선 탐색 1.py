5a006135c5fadce36803c2a8ae3902e3ff8d59fc
hazz <90473086+haaazz@users.noreply.github.com>
2024-04-14 00:12:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 440 ms, Memory: 134336 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+n, m, r = map(int, input().split())
+
+graph = [ [] for _ in range(n+1) ]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+visit = [0] * (n + 1)
+count = 1
+visit[r] = count
+
+def bfs():
+    global count
+    q = deque()
+    q.append(r)
+    while q:
+        now = q.popleft()
+        for i in sorted(graph[now]):
+            if visit[i] == 0:
+                count += 1
+                visit[i] = count
+                q.append(i)
+bfs()
+for i in range(1, n+1):
+    print(visit[i])
